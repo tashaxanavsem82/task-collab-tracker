4fb9006fddc42e187df86e15a95e6d62382d24bd
tashaxanavsem82 <swgwull@hotmail.com>
2025-08-15 06:56:02 -0500

    feat: initial commit for client/src/App.js

---
+++ client/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+const App = () => {
+  return (
+    <div className="container">
+      <h1>Task Collab Tracker</h1>
+      <p>Welcome to the collaborative task tracker!</p>
+    </div>
+  );
+};
+
+export default App;
